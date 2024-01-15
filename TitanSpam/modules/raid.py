0c2cd7b2c3a753ce77db5c8dd93c6d24e2e06c2a
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2024-01-15 20:59:25 +0530

    Commands Panel Updated

--- TitanSpam/modules/raid.py
+++ TitanSpam/modules/raid.py
@@ -2,7 +2,7 @@ import asyncio
 import random
 from telethon import events
 from config import MK1, MK2, MK3, MK4, MK5 , MK6, MK7, MK8, MK9, MK10, SUDO_USERS, OWNER_ID, CMD_HNDLR as hl
-from AltronX.data import RAID, REPLYRAID, ALTRON, MRAID, SRAID, CRAID, ALTRON
+from TitanSpam.data import RAID, REPLYRAID, TITAN, MRAID, SRAID, CRAID, TITAN
 
 que = {}
 
@@ -18,7 +18,7 @@ que = {}
 @MK9.on(events.NewMessage(incoming=True, pattern=r"\%sraid(?: |$)(.*)" % hl))
 @MK10.on(events.NewMessage(incoming=True, pattern=r"\%sraid(?: |$)(.*)" % hl))
 async def spam(e):
-    usage = f"𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲: 𝗥𝗮𝗶𝗱\n  » {hl}raid <count> <Username of User>\n  » {hl}raid <count> <reply to a User>"
+    usage = f"☞ 𝖱𝖺𝗂𝖽 𝖢𝗈𝗆𝗆𝖺𝗇𝖽𝗌 : \n\n1) {hl}raid <count><username/reply> ~ 𝖲𝗍𝖺𝗋𝗍𝗌 𝖠𝖻𝗎𝗌𝗂𝗏𝖾 𝖱𝖺𝗂𝖽 𝗈𝗇 𝖠𝗇𝗒 𝖴𝗌𝖾𝗋."
     if e.sender_id in SUDO_USERS:
         mkraid = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
 
@@ -26,10 +26,10 @@ async def spam(e):
             message = str(mkraid[1])
             a = await e.client.get_entity(message)
             g = a.id
-            if int(g) in ALTRON:
-                await e.reply("» ᴀʀᴇ.. ʏᴇ ᴛᴏ ᴛᴇʀᴀ ʙᴀᴀᴘ ʜᴀɪ", parse_mode=None, link_preview=None)
+            if int(g) in TITAN:
+                await e.reply("☞ 𝖳u Mar Kh𝖺𝗒𝖾𝗀𝖺 𝖡𝖾👻👻", parse_mode=None, link_preview=None)
             elif int(g) in SUDO_USERS:
-                await e.reply("» ᴀʙᴇ.. ʏᴇ sᴜᴅᴏ ʟᴇᴋᴀʀ ʙᴀɪᴛʜᴀ ʜᴀɪ", parse_mode=None, link_preview=None)
+                await e.reply("☞ 𝖠𝖻𝖾 𝖸𝖾 𝖲𝗎𝖽𝗈 𝖶𝖺𝗅𝖺 𝗁𝖺𝗂 𝖡𝗁𝗎𝗍 𝖬𝖺𝗋𝖾𝗀𝖺 👻👻", parse_mode=None, link_preview=None)
             else:
                 c = a.first_name
                 username = f"[{c}](tg://user?id={g})"
@@ -44,10 +44,10 @@ async def spam(e):
             a = await e.get_reply_message()
             b = await e.client.get_entity(a.sender_id)
             g = b.id
-            if int(g) in ALTRON:
-                await e.reply("» ᴀʀᴇ.. ʏᴇ ᴛᴏ ᴛᴇʀᴀ ʙᴀᴀᴘ ʜᴀɪ", parse_mode=None, link_preview=None)
+            if int(g) in TITAN:
+                await e.reply("☞ 𝖳u Mar Kh𝖺𝗒𝖾𝗀𝖺 𝖡𝖾👻👻", parse_mode=None, link_preview=None)
             elif int(g) in SUDO_USERS:
-                await e.reply("» ᴀʙᴇ.. ʏᴇ sᴜᴅᴏ ʟᴇᴋᴀʀ ʙᴀɪᴛʜᴀ ʜᴀɪ", parse_mode=None, link_preview=None)
+                await e.reply("☞ 𝖠𝖻𝖾 𝖸𝖾 𝖲𝗎𝖽𝗈 𝖶𝖺𝗅𝖺 𝗁𝖺𝗂 𝖡𝗁𝗎𝗍 𝖬𝖺𝗋𝖾𝗀𝖺 👻👻", parse_mode=None, link_preview=None)
             else:
                 c = b.first_name
                 counter = int(mkraid[0])
@@ -96,42 +96,42 @@ async def _(event):
 @MK10.on(events.NewMessage(incoming=True, pattern=r"\%srraid(?: |$)(.*)" % hl))
 async def _(e):
     global que
-    usage = f"𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲: 𝐑𝐞𝐩𝐥𝐲𝐑𝐚𝐢𝐝\n  » {hl}rraid <Username of User>\n  » {hl}rraid <reply to a User>"
+    usage = f"☞  𝖱𝖾𝗉𝗅𝗒 𝖱𝖺𝗂𝖽 𝖢𝗈𝗆𝗆𝖺𝗇𝖽𝗌 :\n\n1) {hl}rraid <count><username/reply> ~ 𝖲𝗍𝖺𝗋𝗍𝗌 𝖠𝖻𝗎𝗌𝗂𝗏𝖾 𝖱𝖾𝗉𝗅𝗒 𝖱𝖺𝗂𝖽 𝗈𝗇 𝖠𝗇𝗒 𝖴𝗌𝖾𝗋.\n2) {hl}drraid <count><username/reply> ~ 𝖲𝗍𝗈𝗉𝗌 𝖠𝖻𝗎𝗌𝗂𝗏𝖾 𝖱𝖾𝗉𝗅𝗒 𝖱𝖺𝗂𝖽 𝗈𝗇 𝖴𝗌𝖾𝗋."
     if e.sender_id in SUDO_USERS:
         mkrr = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
         if len(e.text) > 11:
             message = str(mkrr[0])
             a = await e.client.get_entity(message)
             user_id = int(a.id)
-            if int(user_id) in ALTRON:
-                await e.reply("» ᴀʀᴇ.. ʏᴇ ᴛᴏ ᴛᴇʀᴀ ʙᴀᴀᴘ ʜᴀɪ", parse_mode=None, link_preview=None)
+            if int(user_id) in TITAN:
+                await e.reply("☞ 𝖳u Mar Kh𝖺𝗒𝖾𝗀𝖺 𝖡𝖾👻👻", parse_mode=None, link_preview=None)
             elif int(user_id) == OWNER_ID:
-                await e.reply("» ᴀʀᴇ.. ʏᴇ ᴛᴏ ᴛᴇʀᴀ ʙᴀᴀᴘ ʜᴀɪ", parse_mode=None, link_preview=None)
+                await e.reply("☞ 𝖳u Mar Kh𝖺𝗒𝖾𝗀𝖺 𝖡𝖾👻👻", parse_mode=None, link_preview=None)
             elif int(user_id) in SUDO_USERS:
-                await e.reply("» ᴀʙᴇ.. ʏᴇ sᴜᴅᴏ ʟᴇᴋᴀʀ ʙᴀɪᴛʜᴀ ʜᴀɪ", parse_mode=None, link_preview=None)
+                await e.reply("☞ 𝖠𝖻𝖾 𝖸𝖾 𝖲𝗎𝖽𝗈 𝖶𝖺𝗅𝖺 𝗁𝖺𝗂 𝖡𝗁𝗎𝗍 𝖬𝖺𝗋𝖾𝗀𝖺 👻👻", parse_mode=None, link_preview=None)
             else:
                 que[user_id] = []
                 gey = que.get(user_id)
                 phucker = [user_id]
                 gey.append(phucker)
-                await e.reply("» ʜᴇʜᴇ.. ᴀʙ ʀᴇᴘʟʏ ᴋʀɴᴇ ᴘʀ ɢᴀᴀʟɪʏᴀ ᴘᴀʀᴇɢɪ !! ✅", parse_mode=None, link_preview=None)
+                await e.reply("☞ 𝖠𝖺𝖻 𝖬𝖺𝗃𝖺 𝖺𝖺𝗒𝖾𝗀𝖺 😁😁.", parse_mode=None, link_preview=None)
 
         elif e.reply_to_msg_id:             
             a = await e.get_reply_message()
             b = await e.client.get_entity(a.sender_id)
             user_id = int(b.id)
-            if int(user_id) in ALTRON:
-                await e.reply("» ᴀʀᴇ.. ʏᴇ ᴛᴏ ᴛᴇʀᴀ ʙᴀᴀᴘ ʜᴀɪ", parse_mode=None, link_preview=None)
+            if int(user_id) in TITAN:
+                await e.reply("☞ 𝖳u Mar Kh𝖺𝗒𝖾𝗀𝖺 𝖡𝖾👻👻", parse_mode=None, link_preview=None)
             elif int(user_id) == OWNER_ID:
-                await e.reply("» ᴀʀᴇ.. ʏᴇ ᴛᴏ ᴛᴇʀᴀ ʙᴀᴀᴘ ʜᴀɪ", parse_mode=None, link_preview=None)
+                await e.reply("☞ 𝖳u Mar Kh𝖺𝗒𝖾𝗀𝖺 𝖡𝖾👻👻", parse_mode=None, link_preview=None)
             elif int(user_id) in SUDO_USERS:
-                await e.reply("» ᴀʙᴇ.. ʏᴇ sᴜᴅᴏ ʟᴇᴋᴀʀ ʙᴀɪᴛʜᴀ ʜᴀɪ", parse_mode=None, link_preview=None)
+                await e.reply("☞ 𝖠𝖻𝖾 𝖸𝖾 𝖲𝗎𝖽𝗈 𝖶𝖺𝗅𝖺 𝗁𝖺𝗂 𝖡𝗁𝗎𝗍 𝖬𝖺𝗋𝖾𝗀𝖺 👻👻", parse_mode=None, link_preview=None)
             else:
                 que[user_id] = []
                 gey = que.get(user_id)
                 phucker = [user_id]
                 gey.append(phucker)
-                await e.reply("» ᴜғғ.. ᴀʙ ʀᴇᴘʟʏ ᴋʀɴᴇ ᴘʀ ɢᴀᴀʟɪʏᴀ ɴʜɪ ᴘᴀʀᴇɢɪ !! ✅", parse_mode=None, link_preview=None )
+                await e.reply("☞ 𝖠𝖻𝖾 𝗄𝗒𝗎 𝖻𝖺𝗇𝖽 𝗄𝗂𝖺 😢😢.", parse_mode=None, link_preview=None )
         else:
             await e.reply(usage)
 
@@ -147,7 +147,8 @@ async def _(e):
 @MK9.on(events.NewMessage(incoming=True, pattern=r"\%sdrraid(?: |$)(.*)" % hl))
 @MK10.on(events.NewMessage(incoming=True, pattern=r"\%sdrraid(?: |$)(.*)" % hl))
 async def _(e):
-    usage = f"𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲: 𝐃𝐑𝐞𝐩𝐥𝐲𝐑𝐚𝐢𝐝\n  » {hl}drraid <Username of User>\n  » {hl}drraid <reply to a User>"
+    usage = f"☞  𝖱𝖾𝗉𝗅𝗒 𝖱𝖺𝗂𝖽 𝖢𝗈𝗆𝗆𝖺𝗇𝖽𝗌 :\n\n1) {hl}rraid <count><username/reply> ~ 𝖲𝗍𝖺𝗋𝗍𝗌 𝖠𝖻𝗎𝗌𝗂𝗏𝖾 𝖱𝖾𝗉𝗅𝗒 𝖱𝖺𝗂𝖽 𝗈𝗇 𝖠𝗇𝗒 𝖴𝗌𝖾𝗋.\n2) {hl}drraid <count><username/reply> ~ 𝖲𝗍𝗈𝗉𝗌 𝖠𝖻𝗎𝗌𝗂𝗏𝖾 𝖱𝖾𝗉𝗅𝗒 𝖱𝖺𝗂𝖽 𝗈𝗇 𝖴𝗌𝖾𝗋.
+"
     global que    
     if e.sender_id in SUDO_USERS:
         AltX = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
@@ -161,7 +162,7 @@ async def _(e):
                 queue.pop(0)
             except Exception:
                 pass
-            await e.reply("» ᴜғғ.. ᴀʙ ʀᴇᴘʟʏ ᴋʀɴᴇ ᴘʀ ɢᴀᴀʟɪʏᴀ ɴʜɪ ᴘᴀʀᴇɢɪ !! ✅", parse_mode=None, link_preview=None )
+            await e.reply("☞ 𝖠𝖻𝖾 𝗄𝗒𝗎 𝖻𝖺𝗇𝖽 𝗄𝗂𝖺 😢😢.", parse_mode=None, link_preview=None )
         elif e.reply_to_msg_id:             
             a = await e.get_reply_message()
             b = await e.client.get_entity(a.sender_id)
@@ -171,7 +172,7 @@ async def _(e):
                 queue.pop(0)
             except Exception:
                 pass
-            await e.reply("» ᴜғғ.. ᴀʙ ʀᴇᴘʟʏ ᴋʀɴᴇ ᴘʀ ɢᴀᴀʟɪʏᴀ ɴʜɪ ᴘᴀʀᴇɢɪ !! ✅", parse_mode=None, link_preview=None )
+            await e.reply("☞ 𝖠𝖻𝖾 𝗄𝗒𝗎 𝖻𝖺𝗇𝖽 𝗄𝗂𝖺 😢😢.", parse_mode=None, link_preview=None )
         else:
             await e.reply(usage)
 
@@ -187,7 +188,7 @@ async def _(e):
 @MK9.on(events.NewMessage(incoming=True, pattern=r"\%smraid(?: |$)(.*)" % hl))
 @MK10.on(events.NewMessage(incoming=True, pattern=r"\%smraid(?: |$)(.*)" % hl))
 async def spam(e):
-    usage = f"𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲: 𝗠𝗥𝗮𝗶𝗱\n  » {hl}mraid <count> <Username of User>\n  » {hl}mraid <count> <reply to a User>"
+    usage = f"☞  𝖫𝗈𝗏𝖾 𝖱𝖺𝗂𝖽 𝖢𝗈𝗆𝗆𝖺𝗇𝖽𝗌 :\n\n1) {hl}mraid <count><username/reply> ~ 𝖲𝗍𝖺𝗋𝗍𝗌 𝖫𝗈𝗏𝖾 𝖱𝖺𝗂𝖽 𝗈𝗇 𝖠𝗇𝗒 𝖴𝗌𝖾𝗋."
     if e.sender_id in SUDO_USERS:
         mkmr = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
         await e.get_reply_message()
@@ -230,7 +231,7 @@ async def spam(e):
 @MK9.on(events.NewMessage(incoming=True, pattern=r"\%ssraid(?: |$)(.*)" % hl))
 @MK10.on(events.NewMessage(incoming=True, pattern=r"\%ssraid(?: |$)(.*)" % hl))
 async def spam(e):
-    usage = f"𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲: 𝗦𝗥𝗮𝗶𝗱\n  » {hl}sraid <count> <Username of User>\n  » {hl}sraid <count> <reply to a User>"
+    usage = f"☞  𝖲𝗁𝖺𝗒𝖺𝗋𝗂 𝖱𝖺𝗂𝖽 𝖢𝗈𝗆𝗆𝖺𝗇𝖽𝗌 :\n\n1) {hl}sraid <count><username/reply> ~ 𝖲𝗍𝖺𝗋𝗍𝗌 𝖲𝗁𝖺𝗒𝖺𝗋𝗂 𝖱𝖺𝗂𝖽 𝗈𝗇 𝖠𝗇𝗒 𝖴𝗌𝖾𝗋."
     if e.sender_id in SUDO_USERS:
         MKsr = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
         if len(MKsr) == 2:
@@ -273,7 +274,7 @@ async def spam(e):
 @MK9.on(events.NewMessage(incoming=True, pattern=r"\%scraid(?: |$)(.*)" % hl))
 @MK10.on(events.NewMessage(incoming=True, pattern=r"\%scraid(?: |$)(.*)" % hl))
 async def spam(e):
-    usage = f"𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲: C𝗥𝗮𝗶𝗱\n  » {hl}craid <count> <Username of User>\n  » {hl}craid <count> <reply to a User>"
+    usage = f"☞  𝖫𝖾𝗍𝗍𝖾𝗋 𝖱𝖺𝗂𝖽 𝖢𝗈𝗆𝗆𝖺𝗇𝖽𝗌 :\n\n1) {hl}craid <count><username/reply> ~ 𝖲𝗍𝖺𝗋𝗍𝗌 𝖱𝖺𝗇𝖽𝗈𝗆 𝖱𝖾𝗉𝖾𝖺𝗍𝖾𝖽 𝖫𝖾𝗍𝗍𝖾𝗋𝗌 𝖱𝖺𝗂𝖽 𝗈𝗇 𝖠𝗇𝗒 𝖴𝗌𝖾𝗋."
     if e.sender_id in SUDO_USERS:
         MKsr = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
         await e.get_reply_message()
